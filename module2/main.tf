22bd485286a776a37bcd18a3f8d58ffa52799efe
sgandeplli <sgandepalli@softility.com>
2024-10-14 17:30:36 +0530

    Update main.tf

--- module2/main.tf
+++ module2/main.tf
@@ -1,6 +1,7 @@
 provider "google" {
-        credentials = file("/home/hr376/gcp.json")
-        project = "primal-gear-436812-t0"
+  credentials = file("/home/hr376/gcp.json")
+  project     = "primal-gear-436812-t0"
+  region      = "us-central1"
 }
 
 module "my_first_instance" {
